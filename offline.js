2fe05a0917406b2b1459de2929e8ff8bdda3fa44
sahasah <52026752+sahasah@users.noreply.github.com>
2019-09-12 13:31:12 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1568268001,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/plur-sheet0.png",
+		"images/spray-sheet0.png",
+		"images/tree-sheet0.png",
+		"images/tree-sheet1.png",
+		"images/tree-sheet2.png",
+		"images/seed-sheet0.png",
+		"images/water-sheet0.png",
+		"images/cutter-sheet0.png",
+		"images/basket-sheet0.png",
+		"images/ss-sheet0.png",
+		"images/ss-sheet1.png",
+		"images/sprite14-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
